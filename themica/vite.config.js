23e2c39fa09f5da6a3bc175889f96ed999610e19
Samara Silvia <samarasilvia.dev@gmail.com>
2026-03-19 20:15:54 -0300

    Update vite.config.js

--- themica/vite.config.js
+++ themica/vite.config.js
@@ -1,7 +1,8 @@
+// themica/vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/themica/',
+  base: '/',  // ← era '/themica/', muda pra '/'
   build: { outDir: 'dist', assetsDir: 'assets' },
   server: { port: 5174, open: true },
 });
